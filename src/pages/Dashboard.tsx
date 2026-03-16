b09195439ee7b5191dff2fc1233034524fa49475
Biter01 <sebastian.lamplmair@gmail.com>
2026-03-16 16:36:43 +0100

    Added Supabase to project

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,37 @@
+import supabase from "../config/supabaseClient";
+import {use, useEffect, useState} from "react"; 
+
 export default function Dashboard() {
+
+  const [fetchError, setFetchError] = useState<string  | null>(null);
+  const [names, setNames] = useState<any[] | null>(null);
+
+  useEffect(() => {
+    const fetchNames = async () => {
+      const {data, error}  = await supabase 
+        .from('test')
+        .select()
+
+        if(error) {
+          setFetchError('Could not fetch the smoothes');
+          setNames(null)
+          console.log(error)
+        }
+
+        if(data) {
+          setNames(data)
+          setFetchError(null)
+        }
+    }
+
+    fetchNames()
+  },[]);
+
+
   return (
     <div>
-      <h1>Dashboard</h1>
-      <p>Overview of your Smart Home.</p>
+      {fetchError && (<p> {fetchError} </p>)}
+      {names && (<div>{names.map(name => <p>{name.lastname}</p>)}</div>) }
     </div>
   );
 }
